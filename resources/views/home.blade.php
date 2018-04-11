7640060a923af20bd8952d1b70d18f4822c28c82
Vinícius Velasco <sasukegc@gmail.com>
2018-04-10 21:49:55 -0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Home</h1>
+    <p>Chicharrones edison bulb hammock tousled enamel pin raclette deep v. Normcore iPhone vaporware paleo semiotics cornhole. Vexillologist disrupt church-key iceland keffiyeh bitters mlkshk PBR&B. Jean shorts wolf gluten-free tousled gochujang tumeric craft beer shaman, vape tofu.</p>
+@endsection
+
+@section('sidebar')
+    @parent
+    <p>This is appendend to the sidebar</p>
+@endsection
